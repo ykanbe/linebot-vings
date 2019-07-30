0375192fab87afc4f71d766782138659ecb384f9
ykanbe <37165262+ykanbe@users.noreply.github.com>
2019-07-30 14:28:05 +0900

    Update callback.php

--- web/callback.php
+++ web/callback.php
@@ -132,10 +132,10 @@ if ($text == 'はい') {
 } else {
   $response_format_text = [
     "type" => "template",
-    "altText" => "こんにちわ\n何かご用ですか？（はい／いいえ）",
+    "altText" => "こんにちは 何かご用ですか？（はい／いいえ）",
     "template" => [
         "type" => "confirm",
-        "text" => "こんにちわ 何かご用ですか？",
+        "text" => "こんにちは\n何かご用ですか？",
         "actions" => [
             [
               "type" => "message",
